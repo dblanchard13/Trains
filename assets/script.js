7b328ef769166f2bdbd4ada1b527c0c3cdf36372
IamZombiez <dereksiu216@gmail.com>
2017-03-14 10:55:53 -0700

    initial files

--- assets/script.js
+++ assets/script.js
@@ -16,60 +16,71 @@
   $("#submit-Btn").on("click", function(){
   		event.preventDefault();
 
-  		var name = $("#train-Input").val().trim();
-  		var destination = $("#destination-Input").val().trim();
-  		var arrival	= $("#arrival-Input").val().trim();
-  		var freq = $("#freq-Input").val().trim();
+  		var $name = $("#train-Input").val().trim();
+  		var $destination = $("#destination-Input").val().trim();
+  		var $arrival = moment($("#arrival-Input").val().trim();
+  		var $freq = $("#freq-Input").val().trim();
 
 
-  			var newTrain = {
-  			uName: name,
-  			uDestination: destination, 
-  			uArrival: arrival,
-  			uFreq: freq
-  			};
+  		var uTrain = {
+  		uName: name,
+  		uDestination: destination, 
+  		uStart: start,
+  		uFreq: freq
+  		};
 
-  			 database.ref().set(newTrain)
+  		database.ref().push(uTrain);
+
+
+
+  		alert("Train Succesfully Added")
+
+  		$("#train-Input").val("");
+  		$("#destination-Input").val("");
+  		$("#arrival-Input").val("");
+  		$("#freq-Input").val("");
 
    });
 
-  	database.ref().on("value", function(snapshot){
+  
+  	database.ref().on("child_added", function(){
+  	console.log(childSnapshot.val());
+ 	 
+  		var tName = childSnapshot.val().name;
+  		var tDestination = childSnapshot.val().destination;
+  		var tStart = childSnapshot.val().start;
+  		var tFreq = childSnapshot.val().freq;
 
-  		// console.log(snapshot.val());
-  		// console.log(snapshot.val().uName);
-  		// console.log(snapshot.val().uDestination);
-  		// console.log(snapshot.val().uArrival);
-  		// console.log(snapshot.val().uFreq);
 
+  		var nextTrain = 
+  		var minsAway =
 
-  		//Write to the Table
-
-  	    // $("#train-Input").html(snapshot.val().uName);
-  		// $("#destination-Input").html(snapshot.val().uDestination);
-  		// $("#arrival-Input").html(snapshot.val().uArrival);
-  		// $("#freq-Input").html(snapshot.val().uFreq);	
+  		$("#table > tbody").append("<tr><td>" + tName "</td><td>" + tDestination + 
+  		"</td><td>" + tFreq + "</td><td>" + nextTrain + "</td><td>" + minsAway + "</td></tr>");
 
   	});
+
+  
+
 
 
   
 
-  	var firstRowTds = $("table")
-  	.children()
-  	.eq(1)
-	.children("tr")
-	.eq(0)
-	.children("td")
+ //  	var firstRowTds = $("table")
+ //  	.children()
+ //  	.eq(1)
+	// .children("tr")
+	// .eq(0)
+	// .children("td")
 
-	console.log(firstRowTds)
+	// console.log(firstRowTds)
 
-	firstRowTds.eq(0).text("one")
-	firstRowTds.eq(1).text("two")
-	firstRowTds.eq(2).text("three words")
-	firstRowTds.eq(3).text("for you")
-	firstRowTds.eq(4).text("i love you")
-
-
+	// firstRowTds.eq(0).text(snapshot.val().uName);
+	// firstRowTds.eq(1).text(snapshot.val().uDestination);
+	// firstRowTds.eq(2).text(snapshot.val().uFreq);
+	// firstRowTds.eq(3).text()
+	// firstRowTds.eq(4).text()
+	
   	
 
  
@@ -85,8 +96,66 @@
       // firstRowTds.eq(0).text(response.Title);
       // firstRowTds.eq(1).text(response.Year);
       // firstRowTds.eq(2).text(response.Actors);
-  
 
+
+      // append row to the HTML table - creation
+// function appendRow() {
+//     var tbl = document.getElementById('my-table'), // table reference
+//         row = tbl.insertRow(tbl.rows.length),      // append table row
+//         i;
+//     // insert table cells to the new row
+//     for (i = 0; i < tbl.rows[0].cells.length; i++) {
+//         createCell(row.insertCell(i), i, 'row');
+//     }
+// }
+ 
+// // create DIV element and append to the table cell
+// function createCell(cell, text, style) {
+//     var div = document.createElement('div'), // create DIV element
+//         txt = document.createTextNode(text); // create text node
+//     div.appendChild(txt);                    // append text node to the DIV
+//     div.setAttribute('class', style);        // set DIV class attribute
+//     div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+//     cell.appendChild(div);                   // append DIV to the table cell
+// }
+
+// delete table rows with index greater then 0
+// function deleteRows() {
+//     var tbl = document.getElementById('my-table'), // table reference
+//         lastRow = tbl.rows.length - 1,             // set the last row index
+//         i;
+//     // delete rows with index greater then 0
+//     for (i = lastRow; i > 0; i--) {
+//         tbl.deleteRow(i);
+//     }
+// }
+
+// append column to the HTML table
+// function appendColumn() {
+//     var tbl = document.getElementById('table'), // table reference
+//         i;
+//     // open loop for each row and append cell
+//     for (i = 0; i < tbl.rows.length; i++) {
+//         createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
+//     }
+// }
+ 
+// // delete table columns with index greater then 0
+// function deleteColumns() {
+//     var tbl = document.getElementById('my-table'), // table reference
+//         lastCol = tbl.rows[0].cells.length - 1,    // set the last column index
+//         i, j;
+//     // delete cells with index greater then 0 (for each row)
+//     for (i = 0; i < tbl.rows.length; i++) {
+//         for (j = lastCol; j > 0; j--) {
+//             tbl.rows[i].deleteCell(j);
+//         }
+//     }
+// }
+  
+  // $("#arrival-Input") + $("#freq-Input")
+
+//  
 
 
 
